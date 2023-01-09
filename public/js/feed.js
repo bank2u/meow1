9a110a2a85cd8773bd97c7025c89515054859e38
Siwat Jirawiwatpat <siwat.j@b-connex.net>
2023-01-09 10:53:22 +0700

    rename feed.html -> index.html and top rank features

--- public/js/feed.js
+++ public/js/feed.js
@@ -18,15 +18,16 @@ const db = getFirestore(app);
 const txtWeight = document.getElementById('txtWeight');
 const txtFeeder = document.getElementById('txtFeeder');
 const btnSaveWeight = document.getElementById('btnSaveWeight');
+const btnTopFeeder = document.getElementById('btnTopFeeder');
 
 document.addEventListener('DOMContentLoaded', function () {
-    getFeedHistory();
-    getYesterdayTotal();
+    getTodayFeedHistory();
+    getLastWeekFeedHistory();
     txtFeeder.value = getCookie('feeder');
 }, false);
 
 txtFeeder.addEventListener('keyup', (event) => {
-    setCookie('feeder', txtFeeder.value, 31536000 );
+    setCookie('feeder', txtFeeder.value, 31536000);
 });
 
 btnSaveWeight.onclick = async function () {
@@ -39,35 +40,77 @@ btnSaveWeight.onclick = async function () {
     });
     txtWeight.value = 0;
 
-    getFeedHistory();
+    getTodayFeedHistory();
 };
 
-async function getYesterdayTotal() {
-    var date = new Date();
-    date.setDate(date.getDate() - 1);
-    date.setHours(0, 0, 0, 0); // yesterday
+btnTopFeeder.onclick = async function () {
+    getTopFeeder();
+};
 
+async function getLastWeekFeedHistory() {
 
-    var result = await queryFeedHistory(date);
-    var wet = result.filter(x => x.type == 'wet');
-    var dry = result.filter(x => x.type == 'dry');
-    var dryWeight = dry.reduce((total, obj) => obj.weight + total, 0);
-    var wetWeight = wet.reduce((total, obj) => obj.weight + total, 0);
+    const divYesterday = document.getElementById('divHistoryFeedLog');
+    divYesterday.innerHTML = '';
 
-    dryWeight = dryWeight.toFixed(1);
-    wetWeight = wetWeight.toFixed(1);
+    var sDate = new Date();
+    sDate.setDate(sDate.getDate() - 8);
 
-    const divYesterday = document.getElementById('divYesterday');
-    divYesterday.innerHTML = "Yesterday : Dry= " + dryWeight.toString() + "g. , Wet= " + wetWeight.toString() + "g.";
+    var eDate = new Date();
+    eDate.setDate(eDate.getDate() - 1);
+
+    var result = await queryFeedHistoryByDateRange(sDate, eDate);
+    for (var i = 0; i < 7; i++) {
+        var date = sDate;
+        date.setDate(date.getDate() + 1);
+        var date12am = date.setHours(0, 0, 0, 0);
+        var date12pm = date.setHours(23, 59, 59, 999);
+
+        var wet = result.filter(x => x.type == 'wet' && x.date.seconds * 1000 > date12am && x.date.seconds * 1000 < date12pm);
+        var dry = result.filter(x => x.type == 'dry' && x.date.seconds * 1000 > date12am && x.date.seconds * 1000 < date12pm);
+        var dryWeight = dry.reduce((total, obj) => obj.weight + total, 0);
+        var wetWeight = wet.reduce((total, obj) => obj.weight + total, 0);
+
+        dryWeight = dryWeight.toFixed(1);
+        wetWeight = wetWeight.toFixed(1);
+
+        divYesterday.innerHTML += dayjs(date).format("DD/MMM/YYYY") + " : Dry= " + dryWeight.toString() + "g. , Wet= " + wetWeight.toString() + "g. <br/>";
+    }
 }
 
-async function getFeedHistory() {
-    const divFeedLog = document.getElementById('divFeedLog');
-    divFeedLog.innerHTML = '<div class="row">+++ Today +++</div>';
+async function getTopFeeder() {
+
+    const divTopFeeder = document.getElementById('divTopFeeder');
+    divTopFeeder.innerHTML = '';
+
+    var sDate = new Date(2022, 7, 22);
+    var eDate = new Date();
+
+    var result = await queryFeedHistoryByDateRange(sDate, eDate);
+    const feeder = ["Bank", "Fah", "Frong", "Paint", "Fern"];
+
+    feeder.forEach((f) => {
+        var wet = result.filter(x => x.type == 'wet' && x.feeder == f);
+        var dry = result.filter(x => x.type == 'dry' && x.feeder == f);
+
+        var dryWeight = dry.reduce((total, obj) => obj.weight + total, 0);
+        var wetWeight = wet.reduce((total, obj) => obj.weight + total, 0);
+    
+        dryWeight = dryWeight.toFixed(1);
+        wetWeight = wetWeight.toFixed(1);
+    
+        divTopFeeder.innerHTML += f + " : Dry= " + dryWeight.toString() + "g. , Wet= " + wetWeight.toString() + "g. <br/>";
+    });
+
+
+}
+
+async function getTodayFeedHistory() {
+    const divTodayFeedLog = document.getElementById('divTodayFeedLog');
+    divTodayFeedLog.innerHTML = '<div class="row">+++ Today +++</div>';
 
     var todayDate = new Date();
     todayDate.setHours(0, 0, 0, 0);
-    var result = await queryFeedHistory(todayDate);
+    var result = await queryFeedHistoryByDate(todayDate);
 
     var wetSum = 0;
     var drySum = 0;
@@ -83,7 +126,7 @@ async function getFeedHistory() {
         else
             wetSum += weight;
 
-        divFeedLog.innerHTML += '<div class="row">' + date + ' | ' + feeder + ' | ' + fType + ' | ' + weight.toString(); + 'g </div>';
+        divTodayFeedLog.innerHTML += '<div class="row">' + date + ' | ' + feeder + ' | ' + fType + ' | ' + weight.toString(); + 'g </div>';
     });
 
     drySum = drySum.toFixed(1);
@@ -95,12 +138,26 @@ async function getFeedHistory() {
     lblDry.textContent = drySum.toString();
 }
 
-async function queryFeedHistory(date) {
-    var dateEnd = new Date(date.getTime());
-    dateEnd.setHours(23, 59, 59, 999);
+async function queryFeedHistoryByDate(date) {
+    var endDate = new Date(date.getTime());
+    endDate.setHours(23, 59, 59, 999);
     const weightRecord = collection(db, "weight-record");
     const q = query(weightRecord, where("date", ">", date));
-    const q2 = query(q, where("date", "<", dateEnd));
+    const q2 = query(q, where("date", "<", endDate));
+
+    const querySnapshot = await getDocs(q2);
+    return querySnapshot.docs.map(doc => doc.data());
+}
+
+async function queryFeedHistoryByDateRange(startDate, endDate) {
+    var sDate = new Date(startDate.getTime());
+    sDate.setHours(0, 0, 0, 0);
+    var eDate = new Date(endDate.getTime());
+    eDate.setHours(23, 59, 59, 999);
+
+    const weightRecord = collection(db, "weight-record");
+    const q = query(weightRecord, where("date", ">", sDate));
+    const q2 = query(q, where("date", "<", eDate));
 
     const querySnapshot = await getDocs(q2);
     return querySnapshot.docs.map(doc => doc.data());
